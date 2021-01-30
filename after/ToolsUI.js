cf752a23803ab22cefa72538fbcc16f9f0298d65
GrzegorzNecka <strzypior@gmail.com>
2021-01-30 10:44:02 +0100

    1 file modified

--- after/ToolsUI.js
+++ after/ToolsUI.js
@@ -3,15 +3,17 @@ export class ToolsUI {
     const root = this.createRoot();
     this.createButtons(root);
     this.attachToContainer(container, root);
-    this.subscribers = [];
+    this.subscribers = []; //!obserwator
   }
 
+  // stworzenia kontenera na przyciski
   createRoot() {
     const root = document.createElement("div");
     root.classList.add("flex", "flex-column");
     return root;
   }
 
+  // dodajemy przyciski do kontenera
   createButtons(root) {
     root.appendChild(this.createButton("Pencil", "pencil"));
     root.appendChild(this.createButton("Brush", "brush"));
@@ -22,17 +24,34 @@ export class ToolsUI {
     document.querySelector(container).appendChild(root);
   }
 
+  // utworzenie pojedynczego przycisku
   createButton(name, selector) {
     const btn = document.createElement("button");
     btn.setAttribute("data-tool", selector);
     btn.textContent = name;
     btn.addEventListener("click", () => {
-      this.subscribers.forEach(s => s(selector));
+      this.subscribers.forEach(s => s(selector)); //!obserwator
     });
     return btn;
   }
 
-  subscribe(subscriber) {
+  subscribe(subscriber) { //!obserwator
     this.subscribers.push(subscriber);
   }
 }
+
+/**
+ *
+ *    tworzymy reprezentację  w interfejsie użytkownika
+ *
+ *    wzorzec obserwator
+ *      - subscriber - funkcje które nasłuchują na zmianęnarzędzi
+ *      - btn.addEventListener("click", () => jeśli ktośkliknie
+ *        na przycisk to toolsUI rozgłosi żę dany selektor został wybrany
+ *      - ! toolsUI nie wie kto go nasłuchuje - dzieki temu mamy dobrą 
+ *        separację logiki związanej z wyborem narzędzi od logiki związanej 
+ *        z reagowaniem na taką zmianę 
+ *
+ * 
+ * 
+ */
